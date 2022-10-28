0555e8a1cf427ca6345a633adcbc38a078df9a1b
Rikku77 <hagenaarrick@gmail.com>
2022-10-28 11:23:22 +0200

    register and login pages added

---
+++ yc2210-vue/src/views/RegisterView.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+/*
+    const registerForm = document.getElementById("register-form");
+    registerForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let form = e.currentTarget;
+    let url = form.action;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+    let res = fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: jsonData
+    })
+    console.log("submitted");
+})
+*/
+</script>
+<template>
+    <form id="register-form" method="post" action="http://localhost:8080/User">
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="hashedPassword" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+<style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
