571ea6511efdd92734c331af5547a5facec68755
Rikku77 <hagenaarrick@gmail.com>
2022-11-17 20:18:48 +0100

    changed string type

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,16 +1,27 @@
 <script lang="ts">
     import type MovieResultDto from '@/dto/MovieResultDto';
+import type RecipeDto from '@/dto/RecipeDto';
+    import RecipeService from '@/services/RecipeService';
     import { defineComponent } from 'vue';
 
     export default defineComponent({
         name: "results",
         data(){
             return {
-                movie_results: [] as MovieResultDto[]
+                movie_results: [] as MovieResultDto[],
+                recipe_results: [] as RecipeDto[]
             }
         },
         created(){
-            this.movie_results = JSON.parse(localStorage.movie_results)
+            this.movie_results = JSON.parse(localStorage.movie_results);
+
+            for(let i = 0; i< 3; i++){
+                RecipeService.getRandomRecipe()
+                .then(response => this.recipe_results.push(response.data));
+            }
+
+            console.log(this.recipe_results)
+
         }
     })
 </script>
@@ -26,10 +37,11 @@
             <h1 class="title-header">Your 3 selected movies</h1>
             <div class="movie-list">
                 <div class="movie-container" v-for="movie in movie_results">
-                    <img :src="movie.poster" class="image">
+                    <img v-bind:src=movie.poster class="image">
                     <h2>{{movie.title}}</h2>
                 </div>
             </div>
+            <h1 class="title-header">Your 3 selected recipes</h1>
             <div class="recipe-list">
 
             </div>
