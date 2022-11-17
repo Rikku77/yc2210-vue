2d1dbacabb479f155428921ae97631a2bd7f7dab
Rikku77 <hagenaarrick@gmail.com>
2022-11-17 13:29:02 +0100

    Resultsview toegevoegd

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "results",
+        data(){
+            return {
+
+            }
+        }
+    })
+</script>
+<template>
+    hoi
+</template>
+<style>
+
+</style>
