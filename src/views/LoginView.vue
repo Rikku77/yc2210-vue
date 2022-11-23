544eb2d7ff7ab6484fc5c43d5995b6ea1f27d4b5
Rikku77 <hagenaarrick@gmail.com>
2022-11-23 16:08:32 +0100

    Loginview comments deleted

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,8 +3,8 @@
   import type UserLoginDTO from '@/dto/LoginUserDTO';
   import LoginService from "@/services/LoginService";
   import { RouterLink } from "vue-router";
-import type ResponseDto from "@/dto/ResponseDto";
-export default defineComponent({
+  import type ResponseDto from "@/dto/ResponseDto";
+  export default defineComponent({
     name: "login",
     data() {
       return {
@@ -25,30 +25,6 @@ export default defineComponent({
       }
     }
   })
-  /*
-  const loginForm = document.getElementById("login-form");
-  loginForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  let form = e.currentTarget;
-  let url = form.action;
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-  const jsonData = JSON.stringify(data);
-  console.log(jsonData);
-  let res = fetch(url, {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      },
-      body: jsonData
-  })
-  console.log("submitted")
-})
-*/
-
-
-
 </script>
 
 <template>
@@ -66,7 +42,6 @@ export default defineComponent({
         <nav class="text-center">
           <RouterLink to="/register">Register</RouterLink>
         </nav>
-
     </form>
     <RouterLink to="/landing" custom v-slot="{ navigate }">
       <button type="button" class="btn btn-secondary" @click="navigate" role="link"> Continue as guest </button>
